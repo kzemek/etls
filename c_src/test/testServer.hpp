a569a7b2da2383f0fb50d32695f27624dc779031
Konrad Zemek <konrad.zemek@gmail.com>
2015-05-21 15:49:05 +0200

    VFS-1126 Implement a basic test server.

--- c_src/test/testServer.hpp
+++ c_src/test/testServer.hpp
@@ -9,4 +9,42 @@
 #ifndef ERLANG_TLS_TEST_SERVER_HPP
 #define ERLANG_TLS_TEST_SERVER_HPP
 
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ssl/context.hpp>
+#include <boost/asio/ssl/stream.hpp>
+#include <boost/asio/spawn.hpp>
+
+#include <atomic>
+#include <chrono>
+#include <memory>
+#include <thread>
+#include <vector>
+
+class TestServer {
+    using SSLSocket = boost::asio::ssl::stream<boost::asio::ip::tcp::socket>;
+
+public:
+    TestServer(const unsigned short port);
+    ~TestServer();
+
+    bool waitForConnections(
+        std::size_t number, std::chrono::milliseconds timeout);
+
+    void send(boost::asio::const_buffer buffer);
+    void receive(boost::asio::mutable_buffer buffer);
+
+private:
+    void startAccept(boost::asio::yield_context yield);
+
+    boost::asio::io_service m_ioService;
+    boost::asio::io_service::work m_work;
+    boost::asio::ip::tcp::acceptor m_acceptor;
+    boost::asio::ssl::context m_context;
+    std::thread m_thread;
+    std::vector<std::unique_ptr<SSLSocket>> m_sessions;
+
+    std::atomic<std::size_t> m_sessionsSize{0};
+};
+
 #endif // ERLANG_TLS_TEST_SERVER_HPP
