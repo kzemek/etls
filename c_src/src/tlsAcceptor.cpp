430d039c53df5d7747bd0470a34ff8ade02390ee
Konrad Zemek <konrad.zemek@gmail.com>
2016-07-21 20:18:49 +0200

    Take weak reference when accepting on listen sock.
    Calling accept() on the listen socket would prevent it from
    being destroyed until a connection has actually been accepted.

--- c_src/src/tlsAcceptor.cpp
+++ c_src/src/tlsAcceptor.cpp
@@ -30,19 +30,23 @@ void TLSAcceptor::acceptAsync(Ptr self, Callback<TLSSocket::Ptr> callback)
         =, self = std::move(self), callback = std::move(callback)
     ]() mutable {
         auto sock = std::make_shared<TLSSocket>(m_app, *this);
-        m_acceptor.async_accept(sock->m_socket.lowest_layer(),
-            [ =, self = std::move(self), callback = std::move(callback) ](
-                                    const auto ec) mutable {
-                if (ec) {
-                    callback(ec);
-                }
-                else {
-                    sock->m_socket.lowest_layer().set_option(
-                        asio::ip::tcp::no_delay{true});
+        m_acceptor.async_accept(sock->m_socket.lowest_layer(), [
+            =, s = std::weak_ptr<TLSAcceptor>{self},
+            callback = std::move(callback)
+        ](const auto ec) mutable {
+            if (ec) {
+                callback(ec);
+            }
+            else if (auto self2 = s.lock()) {
+                sock->m_socket.lowest_layer().set_option(
+                    asio::ip::tcp::no_delay{true});
 
-                    callback(sock);
-                }
-            });
+                callback(sock);
+            }
+            else {
+                callback(std::make_error_code(std::errc::operation_canceled));
+            }
+        });
     });
 }
 
